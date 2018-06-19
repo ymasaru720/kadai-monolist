b6ae6cefcb876af19339490ad12989aa376d4cdb
masaru.yamaguchi <ec2-user@ip-172-31-11-70.us-east-2.compute.internal>
2018-06-19 04:55:15 +0000

    have ranking

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -23,6 +23,7 @@
                         @if (isset($item->count))
                             <div class="panel-footer">
                                 <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Wants</p>
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Have</p>
                             </div>
                         @endif
                     </div>
